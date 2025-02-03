13a05f57d8191e02893a6ae397b95f045716c51a
Kedar-Khati <khatikm_1@rknec.edu>
2025-02-04 00:56:52 +0530

    css to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from './components/Home';
 import Connect from './components/Connect';
+import { Community } from "./components/Community";
+import { Companies } from "./components/Companies";
+import { About } from "./components/About";
 
 export default function App() {
   return (
@@ -14,6 +17,9 @@ export default function App() {
     <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/connect" element={<Connect/>}></Route>
+        <Route path="/community" element={<Community/>}></Route>
+        <Route path="/companies" element={<Companies/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
     </Routes>
   </>
   );
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,26 +1,53 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/clerk-react'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/Dashboard.css';
 
 export const Dashboard = () => {
   return (
-    <div className='flex font-bold justify-between p-5'>
-      <div className='w-100 flex justify-evenly'>
-          <NavLink to='/'>
-            logo
-          </NavLink>
-          <NavLink to='/connect'>
-            Connect
-          </NavLink>
+    <div className='dashboard-container'>
+
+      {/* Logo Section */}
+      <div id='logo-div'>
+        <NavLink to='/'>Logo</NavLink>
       </div>
-      <div>
+
+      {/* Search Box Section */}
+      <div id="search-box">
+        <form className="search-form">   
+            <label htmlFor="default-search" className="sr-only">Search</label>
+            <div className="search-wrapper">
+                <input 
+                  type="search" 
+                  id="default-search" 
+                  className="search-input" 
+                  placeholder="Search..." 
+                  required 
+                />
+                <button type="submit" className="search-button">Search</button>
+            </div>
+        </form>
+      </div>
+
+      {/* Navigation Links Section */}
+      <div id="nav-div">
+        <NavLink to='/connect'>Connect</NavLink>
+        <NavLink to='/community'>Community</NavLink>
+        <NavLink to='/companies'>Companies</NavLink>
+        <NavLink to='/about'>About</NavLink>
+      </div>
+
+      {/* Authentication Buttons */}
+      <div className='auth-buttons'>
         <SignedOut>
-          <SignInButton></SignInButton>
+          <SignInButton />
         </SignedOut>
         <SignedIn>
-          <SignOutButton></SignOutButton>
+          <SignOutButton />
+          <UserButton className="custom-user-button"/>
         </SignedIn>
       </div>
+
     </div>
-  )
-}
+  );
+};
